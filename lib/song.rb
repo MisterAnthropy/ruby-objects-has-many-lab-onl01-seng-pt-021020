9a513a5193baacdcb947590416897a072cb39d7f
Tyler Croley <tjcroley@outlook.com>
2020-03-09 22:51:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,15 @@
-class Song 
-  attr_accessor :name, :artist  
- 
-  
+class Song
+  attr_accessor :name, :artist
+
   def initialize(name)
     @name = name
   end
+
   def artist_name
-    if @artist == nil 
-      return nil 
+    if @artist == nil
+      return nil
     else
-      artist.name
+      @artist.name
     end
   end
 end
