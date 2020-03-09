9dbf8c912f05d8c51b94a3ae1ad9aea63d80dc3d
Tyler Croley <tjcroley@outlook.com>
2020-03-09 22:41:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,16 @@
 class Artist 
   attr_accessor :name, :songs 
-  @song_count = 0 
+  
+  @@song_count = 0 
+  
   def initialize(name)
     @name = name 
     @songs = []
   end
+  
   def add_song(song)
-    song.artist = self
+    @songs << song 
+    
   
   end
 
