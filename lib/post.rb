8f1eaae844df5f14bd6dbc3c11b822580344ef14
Tyler Croley <tjcroley@outlook.com>
2020-03-09 22:59:58 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,15 +1,24 @@
 class Post
-
+  
   attr_accessor :title, :author
+  
+  @@all = []
+  
   def initialize(title)
     @title = title
+    @@all << self
   end
-
+  
+  def self.all
+    @@all
+  end
+  
   def author_name
-    if @author == nil
-      return nil
+    if self.author.nil?
+      nil
     else
-      @author.name
+      self.author.name
     end
   end
+  
 end
